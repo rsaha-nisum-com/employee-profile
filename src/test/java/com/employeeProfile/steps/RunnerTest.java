059f8e136a80ea4999ca702c486929e4e1c3156d
NisumUser <NIS1600@NisumUsersMBP8.gap.com>
2016-05-04 14:28:47 -0700

    removed whitespace

---
+++ src/test/java/com/employeeProfile/steps/RunnerTest.java
@@ -0,0 +1,13 @@
+package com.employeeProfile.steps;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = {"pretty"})
+
+public class RunnerTest {
+
+}
